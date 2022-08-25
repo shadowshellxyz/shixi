42ef2b1f18dc4927e7d8c5bf338be2a5fd654737
shadow <shadowdefendernet@qq.com>
2022-08-25 10:38:08 +0800

    auto backup, timestamp is 2022-08-25 10:38:01.

--- cmds/daily.sh
+++ cmds/daily.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 timestamp=`date "+%Y-%m-%d %H:%M:%S"`
+commit_msg="auto backup, timestamp is $timestamp."
 # update notes
 pull_note(){
     cd $JARVIS_HOME/Shadow/Note
@@ -12,14 +13,14 @@ pull_note(){
 push_note(){
     cd $JARVIS_HOME/Shadow/Note
     git add -A
-    git commit -m 'auto backup -> $timestamp'
+    git commit -m $commit_msg
     git push origin main
 }
 
 push_jarvis(){
     cd $JARVIS_HOME/Shadow/jarvis
     git add -A
-    git commit -m 'auto backup -> $timestamp'
+    git commit -m $commit_msg
     git push origin main
 }
 
