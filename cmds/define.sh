a2aa6b1d4169ca47550460b9e14106060281a2d8
shadow <shadowdefendernet@qq.com>
2022-08-25 10:35:16 +0800

    auto backup -> $timestamp

--- cmds/define.sh
+++ cmds/define.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 
+# execute command
+execute_command(){
+    handler=${routing_table["$*"]}
+    echo "command is '$*', handler is '$handler'."
+    `$handler`
+}
+
+# alias command
+alias jarvis=execute_command
+
 # source files
 source $JARVIS_COMMANDS_PATH/root.sh
 
 # define commands
-alias jarvis=execute_command
-
-execute_command(){
-    #echo $*
-    if [ "$*" = "ready" ]
-    then
-        jarvis_ready
-    elif [ "$*" = "bye" ]
-    then
-        jarvis_bye
-    elif [ "$*" = "date" ]
-    then
-        echo "Hello shadow, now is `date "+%Y-%m-%d %A %H:%M:%S".`"
-    elif [ "$*" = "start work" ]
-    then
-        jarvis_start_work
-    elif [ "$*" = "complete work" ]
-    then
-        jarvis_complete_work
-    elif [ "$*" = "read excel" ]
-    then
-        python3 $JARVIS_COMMANDS_PATH/readexcel.py
-    elif [ "$*" = "write excel" ]
-    then
-        python3 $JARVIS_COMMANDS_PATH/writeexcel.py
-    else 
-        echo "Invalid command : $*."
-    fi
-}
+declare -A routing_table
+routing_table["morning"]="jarvis_ready"
+routing_table["evening"]="jarvis_bye"
+routing_table["date"]="Hello shadow, now is `date "+%Y-%m-%d %A %H:%M:%S".`"
+routing_table["start work"]="jarvis_start_work"
+routing_table["complete work"]="jarvis_complete_work"
+routing_table["read excel"]="python3 $JARVIS_COMMANDS_PATH/readexcel.py"
+routing_table["write excel"]="python3 $JARVIS_COMMANDS_PATH/writeexcel.py"
