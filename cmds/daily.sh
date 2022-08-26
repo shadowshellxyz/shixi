9402eb383e1fa3f990ef2b8961e48eebe2c47c86
shadow <shadowdefendernet@qq.com>
2022-08-26 12:19:02 +0800

    20220826121855 auto backup.

--- cmds/daily.sh
+++ cmds/daily.sh
@@ -3,6 +3,11 @@
 timestamp=`date "+%Y%m%d%H%M%S"`
 commit_msg="$timestamp auto backup."
 
+refresh_git_config(){
+    git config user.name shadow
+    git config user.email shadowdefendernet@qq.com
+}
+
 # update notes
 pull_note(){
     cd $JARVIS_HOME/Shadow/Note
@@ -13,6 +18,7 @@ pull_note(){
 # save notes
 push_note(){
     cd $JARVIS_HOME/Shadow/Note
+    refresh_git_config
     git add -A
     git commit -m $commit_msg
     git push origin main
@@ -20,6 +26,7 @@ push_note(){
 
 push_jarvis(){
     cd $JARVIS_HOME/Shadow/jarvis
+    refresh_git_config
     git add -A
     git commit -m $commit_msg
     git push origin main
